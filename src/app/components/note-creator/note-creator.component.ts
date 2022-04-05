65ebb42fcfabef96ca01bb82f4bf41875e3ea8a0
Daniel Ibarra <dih_nes@yahoo.com.mx>
2022-04-05 14:13:10 -0500

    commit

--- src/app/components/note-creator/note-creator.component.ts
+++ src/app/components/note-creator/note-creator.component.ts
@@ -13,21 +13,29 @@ export class NoteCreatorComponent implements OnInit {
   @Input() width!: string;
 
   current: any
+  userInfo!: UserModel
 
   constructor(private session: SessionService, private graphql: GraphqlService) {
-    this.current = JSON.parse(this.session.currentUser())    
+    this.current = JSON.parse(this.session.currentUser())
+    this.getUserData()
   }
 
-  ngOnInit(): void {
-    this.userData()
+  ngOnInit() {
+    console.log(this.userInfo);
   }
 
   hasPermission() {
     return this.current.internal
   }
 
-  userData() {
-    this.graphql.getUserById(this.current.id).subscribe
+  private getUserData() {
+    this.graphql.getUserById(this.current.id).subscribe(({ data, loading }) => {
+      if (data) {
+        if (!loading) {
+          this.userInfo = data as UserModel
+        }
+      }
+    })
   }
 
 }
